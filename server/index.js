096912f440049a1cf0cc0e4b7d3c2ee79e8aacc2
yusluv1234 <yusluv124@gmail.com>
2023-03-28 16:01:18 +0100

    Adding dotenv folder

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import multer from "multer";
 import path from "path";
@@ -10,12 +11,12 @@ import { fileURLToPath } from "url";
 
 /*  CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirtname(__filename);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app =  express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -32,3 +33,12 @@ const storage = multer.diskStorage({
   }
 })
 const upload = multer({ storage })
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifieldTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
